a8f989d1a548e822ca44f597bc39ea5b1fc1d130
antmachine <anthonysallows@gmail.com>
2014-07-18 07:09:34 -0700

    Moved directories

--- ritly/config/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-  root to: 'urls#index'
-  # get 'urls/new', to: 'urls#new'
-  # post 'urls/create', to: 'urls#create'
-  resources :urls
-  get 'go/:random_string', to: 'urls#go'
-end
